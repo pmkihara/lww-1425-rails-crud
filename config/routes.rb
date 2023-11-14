f1be0b1f83e1c048e07e6f1ae5a5e3234b415e35
Patricia Kihara <patricia.kihara@outlook.com>
2023-11-14 11:54:09 -0300

    feat: implement crud

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Listar todos os restaurantes
+  get 'restaurants', to: 'restaurants#index'
+  # Criar um restaurante novo
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+  # Ver os detalhes de um restaurante
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  # Editar um restaurante
+  get 'restaurants/:id/edit', to: 'restaurants#edit'
+  patch 'restaurants/:id', to: 'restaurants#update'
+  # Apagar um restaurante
+  delete 'restaurants/:id', to: 'restaurants#destroy'
 end
